564e69ee99401df0d48c4efb0c22d26069d6f02d
Keunyong Kim <keunykim@c2r2s6.42gyeongsan.kr>
2024-02-26 20:29:54 +0900

    qwe

--- 연습장.c
+++ 연습장.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
 int main(){
-    printf("c언어");
+    int a;
+    a = 19033;
+
+    // Y의 아스키코드는 89
+    // J의 아스키코드는 74
+    write(1, &a, 2);
+
     return 0;
 }
