a27b801b28b9ad9fc093c9d14e336a0e23577ae5
Keunyong Kim <keunykim@c3r15s2.42gyeongsan.kr>
2024-03-17 09:43:44 +0900

    gg

--- 연습장/c.c
+++ 연습장/c.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-	printf("%p	\n", (void*) 0);
-	printf("%p	\n", (void*) 1);
-	printf("%p	\n", (void*) 2);
-	printf("%p	\n", (void*) 3);
+typedef struct A_{
+	int a;
+	int b;
+} A;
 
-	printf("%p	\n", (int*) 0);
-	printf("%p	\n", (int*) 1);
-	printf("%p	\n", (int*) 2);
-	printf("%p	\n", (int*) 3);
+int main() {
+	A w;
+	w.a = 1;
+	w.b = 2;
+	printf("%d, %d", w.a, w.b);
 }
