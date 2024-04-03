4f23dc1a66092314340ec151c5dc1035c72bb9c1
Keunyong Kim <keunykim@c2r2s3.42gyeongsan.kr>
2024-04-03 17:41:09 +0900

    Wed_Apr__3_05:41:09

--- 42/config/config.fish
+++ 42/config/config.fish
@@ -12,7 +12,8 @@ if status is-interactive
 	alias R="clear; cd $42/rendu/$NAME; pwd; ls";
 	alias e="clear; cd $dev/exercise; pwd; ls";
 
-	alias g="clear; git add .; git commit -m 'commit'; git push";
+	alias g="clear; git add .; date | sed 's/ [AP]M.\+//' | tr ' ' '_' | xargs git commit -m
+; git push";
 
 	alias c="clear; cc $FLAGS *.c -o $out";
 	alias sc="clear; cc $FLAGS $42/integrate/$NAME.c -o $out";
