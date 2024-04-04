e582a772b43f65f1df973f9760d4d239372217ae
Keunyong Kim <keunykim@c2r2s3.42gyeongsan.kr>
2024-04-04 18:06:53 +0900

    Thu_Apr__4_06:06:53

--- 42/finish/libft_mandatory/libft.h
+++ 42/finish/libft_mandatory/libft.h
@@ -6,7 +6,7 @@
 /*   By: keunykim <keunykim@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 23:52:52 by keunykim          #+#    #+#             */
-/*   Updated: 2024/03/19 19:13:50 by keunykim         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:34:21 by keunykim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,22 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
+
+typedef struct s_list
+{
+    void *content;
+    struct s_list *next;
+}  t_list;
+
+t_list              *ft_lstnew(void *content);
+int                 ft_lstsize(t_list *lst);
+t_list              *ft_lstlast(t_list *lst);
+void                ft_lstadd_front(t_list **lst, t_list *new);
+void                ft_lstadd_back(t_list **lst, t_list *new);
+void                ft_lstdelone(t_list *lst, void (*del)(void *));
+void                ft_lstclear(t_list **lst, void (*del)(void *));
+void                ft_lstiter(t_list *lst, void (*f)(void *));
+t_list              *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+
 #endif
