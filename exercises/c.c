e5cb0198ecbb3089f8350da36e1c8b55ee083380
Keunyong Kim <keunykim@c2r2s3.42gyeongsan.kr>
2024-04-05 12:02:59 +0900

    Fri_Apr__5_12:02:59

--- exercises/c.c
+++ exercises/c.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int	main(void)
 {
-	char buf[1 + 13 * 2];
-	int i;
-
-	i = -1;
-	while(++i + 'a' <= 'z')
-		buf[i] = i + 'a';
-	buf[i] = 0;
-	printf("%s", buf);
-	return (0);
+	printf("abcdef");
 }
