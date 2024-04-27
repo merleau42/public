990725b3ef659ee52c31a09b8e3fd7f3335542ff
Keunyong Kim <keunykim@c2r2s2.42gyeongsan.kr>
2024-04-27 18:57:29 +0900

    Sat_Apr_27_06:57:29

--- exercises/c.c
+++ exercises/c.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-	int a,b;
-	scanf("%d %d", &a, &b);
-	printf("%.10f", (double)a / b);
+	const char *base[3] = {"123", "456", "789"};
+	printf("%s", base[0]);
+	printf("%s", base[1]);
+	printf("%s", base[2]);
 }
