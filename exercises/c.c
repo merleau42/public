b1ad430cd7705bd8c4f17b2f74a8ede1d20f6c0b
Keunyong Kim <keunykim@c2r11s2.42gyeongsan.kr>
2024-05-01 06:36:41 +0900

    Wed_May__1_06:36:41

--- exercises/c.c
+++ exercises/c.c
@@ -2,8 +2,9 @@
 
 int main()
 {
-	const char *base[3] = {"123", "456", "789"};
-	printf("%s", base[0]);
-	printf("%s", base[1]);
-	printf("%s", base[2]);
+	printf("ABC");
+	printf("DEF");;
+	printf("GHI");;;
+	printf("JKL");;;;
+	printf("MNO");;;;;
 }
