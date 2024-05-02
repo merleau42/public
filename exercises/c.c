b1b1081f7a84ef125e63830fb8c8e14412d46d78
Keunyong Kim <keunykim@c2r2s3.42gyeongsan.kr>
2024-05-02 16:06:39 +0900

    Thu_May__2_04:06:39

--- exercises/c.c
+++ exercises/c.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char **free_log;
+
+void smart_free(void* any)
+{
+	size_t i = 1;
+
+	free_log[0]++;
+	free_log[(size_t) free_log[0]] = (char*) any;
+	free(any);
+}
 
 int main()
 {
-	printf("ABC");
-	printf("DEF");;
-	printf("GHI");;;
-	printf("JKL");;;;
-	printf("MNO");;;;;
+	free_log = (char **) malloc(1000 * sizeof(char*));
+
+	int *m, *n, *k;
+	m = (int *) malloc(5 * sizeof(int));
+	n = (int *) malloc(12 * sizeof(int));
+	k = (int *) malloc(9 * sizeof(int));
+
+	// smart_free(m);
+	// smart_free(n);
+	// smart_free(k);
+
+	// size_t i = 1;
+	// while(i <= (size_t) free_log[0])
+	// 	printf("%p\n", free_log[i++]);
+
+	free(m);
+	free(n);
+	free(m);
 }
+
