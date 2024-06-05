83e662cc6d0215678636b77472c2f4761aa5794d
메를로 <keunykim@c2r2s3.42gyeongsan.kr>
2024-06-05 20:06:53 +0900

    Wed_Jun__5_08:06:53

--- 프로젝트/개발자도구/인트라.js
+++ 프로젝트/개발자도구/인트라.js
@@ -1,4 +1,4 @@
-cadet = ["ahrelee","bohchoi","bonikoo","chan-ypa","changhyu","changwpa","chjeong","chunpark","dae-lee","daeekim","djang","dohukim","dohyeki2","dohyuki2","doji","dojkoo","dongcho","donghwi2","donjeong","donjung","donkim3","doyoukim","dukim","eonoh","esong","eunhwang","geonyole","gihokim","gijung","gseok","haeem","hchin","hdoo","hisong","hocjeong","hoysong","huylee","hwasong","hyebinle","hyenchoi","hyeojeon","hyeyeom","hyunahn","hyungyki","hyunjiki","inam","iyeo","jacha","jaegbae","jaehukim","jaemjeon","jahong","jajo","jakim","jehbae","jemoon","jheo","jihamoon","jihwkim2","jimjeong","jinkwon","jinsecho","jinseo","jinyjeon","jishin","jisopark","joojeon","joonchoi","juheelee","juhyekim","junghule","junhoh","junkwak","junlee","junmin","junseyun","jupyo","kchoo","keunykim","kjung","ksuh","kyouhele","minju","mku","myeochoi","sanbaek","sangseo","sangwook","sejjeong","seku","seojang","seokjile","seongkim","seongmip","seonhwan","seoyokim","seungjo","siyukim","sooslee","ssohn","sunghyki","sunhnoh","suwi","taegkang","taehykwo","taejikim","taepark","wonhseo","woonhan","wooslee","wsunwoo","yeoju","yeojukim","yeonlee","yerpark","yikim2","yokoh","younam","younhbae","yuhjeon","yuhyoon","yuna","yunseo","yutsong","ahrelee"]
+cadet = ["ahrelee","bohchoi","bonikoo","chan-ypa","changhyu","changwpa","chjeong","chunpark","dae-lee","daeekim","djang","dohukim","dohyeki2","dohyuki2","doji","dojkoo","dongcho","donghwi2","donjeong","donjung","donkim3","doyoukim","dukim","eonoh","esong","eunhwang","geonyole","gihokim","gijung","gseok","hchin","hdoo","hisong","hocjeong","hoysong","huylee","hwasong","hyebinle","hyenchoi","hyeojeon","hyeyeom","hyunahn","hyungyki","hyunjiki","inam","iyeo","jacha","jaegbae","jaehukim","jaemjeon","jahong","jajo","jakim","jehbae","jemoon","jheo","jihamoon","jihwkim2","jimjeong","jinkwon","jinsecho","jinseo","jinyjeon","jishin","jisopark","joojeon","joonchoi","juheelee","juhyekim","junghule","junhoh","junkwak","junlee","junmin","junseyun","jupyo","kchoo","keunykim","kjung","ksuh","kyouhele","minju","mku","myeochoi","sanbaek","sangseo","sangwook","sejjeong","seku","seojang","seokjile","seongkim","seongmip","seonhwan","seoyokim","seungjo","siyukim","sooslee","ssohn","sunghyki","sunhnoh","suwi","taegkang","taehykwo","taejikim","taepark","wonhseo","woonhan","wooslee","wsunwoo","yeoju","yeojukim","yeonlee","yerpark","yikim2","yokoh","younam","younhbae","yuhjeon","yuhyoon","yuna","yunseo","yutsong","ahrelee"]
 
 try{if (tick ==  5) here=document.location.href.match(/.+\/users\/(.+)/)[1];
 	if (tick == 10) next=cadet[cadet.indexOf(here)+1];
@@ -6,6 +6,19 @@ try{if (tick ==  5) here=document.location.href.match(/.+\/users\/(.+)/)[1];
 	if (tick == 30) document.location.href = document.location.href = document.location.href.replace(/.+?users\/(.+)/, next);
 tick++;}
 catch{tick = 0}
+
+'huylee 겟본'
+'junseyun 프겟'
+'chjeong 프겟'
+'sangseo 프겟'
+'esong 프겟'
+'jehbae 프겟'
+'taegkang 프겟'
+'geonyole 겟본'
+'hyunjiki 프'
+'sanbaek 프'
+'keunykim'
+'inam'
 
 
 /* 마지막 문장만 수정하면 됨 */
