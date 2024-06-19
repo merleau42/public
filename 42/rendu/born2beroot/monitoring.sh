9ec0e0e4494f11ee3dcb4eb672b33a682b265683
메를로 <keunykim@c2r2s3.42gyeongsan.kr>
2024-06-19 11:15:40 +0900

    Wed_Jun_19_11:15:40

--- 42/rendu/born2beroot/monitoring.sh
+++ 42/rendu/born2beroot/monitoring.sh
@@ -1,4 +1,5 @@
 vi /root/monitoring.sh
+apt install sysstat
 
 # 현재 시스템의 아키텍처 정보를 출력하기
 # uname ㅡ 커널(Linux) 호스트(kky) 배포판(6.1.0-amd) 커널버전(#상세) 아키텍쳐(x86_64) 운영체제(GNL/Linux)
@@ -23,23 +24,19 @@ free --mebi | grep Mem | awk '{printf "%d/%dMB (%.2f%%)\n", $3, $2, $3/$2 * 100}
 printf "#Disk Usage: "
 df | grep /dev/mapper/ | awk '{used+=$3; cap+=$2} END {printf "%d/%dGb (%d%%)\n", used/1024, cap/1024^2, used/cap*100}'
 
-#mpstat은 각 CPU 코어의 사용률과 각 코어의 평균 사용률을 보여주는 명령어이다.
-#$13은 CPU 사용률을 의미한다. 즉 100-$13은 CPU 유휴 시간의 백분율을 표시하는 것이다.
-# apt install sysstat
+# mpstat은 각 CPU 코어의 사용률을 보여주는 명령어. 마지막 idle은 놀고있는 비율을 의미함.
 printf "#CPU load: "
 mpstat | grep all | awk '{printf "%.2f%%\n", 100-$NF}'
 
-# who는 현재 시스템에 로그인한 사용자를 보여주는 명령어이다. -b옵션은 시스템 부팅 시간을 보여주는 명령어이다. 여기엔 "system boot"라는 문자열이 포함되어 있다.
-# 이를 해결하기 위해 sed명령어를 사용하는 것이며 해당 명령어는 원하는 부분만 출력 및 변호나하는데 사용한다. 여기선 공백으로 치환하는 역할을 한다
+# who는 현재 시스템에 로그인한 사용자를 보여주는 명령어. -b옵션은 시스템 부팅 시간을 보여줌.
 printf "#Last boot: "
 who -b | sed -E 's/\s+system boot\s+//'
 
-# lsblk는 lvm을 사용하는 블록 디바이스가 있는지 확인하는 것이다.
-# -gt은 왼쪽에 있는 값이 오른쪽에 있는 값보다 크면 참을 반환하는 비교 연산자이다.
+# lsblk에서 lvm에 해당하는 라인 갯수를 셈
 printf "#LVM use: "
 lsblk | grep lvm | wc -l | sed -E 's/.*[1-9].*/yes/' | sed -E 's/0+/no/'
 
-# ss는 소켓 정보를 보여주는 명령어이다. -tunpl은 TCP/UDP 소켓 정보를 상세히 보여주는 옵션이다.
+# ss --tcp는 TCP 연결을 보여줌. 이 중에서 올바르게 승인된 것을 ESTAB 이라고함.
 printf "#Connections TCP : "
 ss --tcp | grep ESTAB | wc -l | awk '{printf "%d ESTABLISHED\n", $1}'
 
@@ -50,25 +47,20 @@ who | wc -l
 # 현재시스템의 IP 주소를 개행 없이 출력하는 명령어이다.
 # ip link show는 시스템에 설치된 네트워크 인터페이스의 정보를 출력하는 명령어이다.
 # 그 후는 출력된 결과 중에서, 첫 번째 필드가 "link/ether"인 라인의 두번째 필드를 출력한다. 여기서 "link/ether"는 이더넷 인터페이스의 물리적 주소(MAC 주소)를 나타내는 키워드이다.
-printf "#Network: IP ";
+printf "#Network: IP "
 hostname -I | awk '{printf "%s (", $1}'
 ip link show | awk '$1 == "link/ether" {print $2}' | sed '2, $d' | tr -d '\n'
 printf ")\n"
 
-# journalctl은 systemd의 로그 시스템인 journald에서 저장된 로그를 검색하고 출력하는 유틸리티이다. 이는 전통적인 로그 파일 대신 바이너리 파일 형식으로 로그를 저장하고, 이를 검색하기 위해 journalctl을 사용할 수 있다.
-# _COMM=sudo를 추가함으로써 _COMM필드가 sudo인 로그 메시지를 검색할 수 있도록 한다.
+# journalctl을 써보려고 했지만, 불필요한 라인을 걸러내야해서 다른 방법을 찾아봄.
 printf "#Sudo : "
-journalctl _COMM=sudo | wc -l | tr -d '\n'
-printf " cmd\n"
-
+ls -lR /var/log/sudo | grep 'log$' | wc -l | awk '{printf "%d cmd\n", $1}'
 
 
 
 ###############################
 
-
-
-#파일에 실행 권한을 추가하는 명령어이다.
+#파일에 실행 권한을 추가
 chmod +x monitoring.sh 
 
 # cron은 리눅스에서 백그라운드에서 동작하는 작업 스케줄러이다.
@@ -76,30 +68,21 @@ chmod +x monitoring.sh
 crontab -e
 */10 * * * * /root/monitoring.sh | wall
 
-
-
 ##############################
 groupadd user42
 usermod -aG sudo,user42 <사용자이름>
 usermod -g user42 <사용자이름>
 
-
 vm을 상태 저장하면서 종료
 스냅샷을 남긴다.
-터미널에서 cd /goinfre/intra id/vm이름 으로 가상머신이 설치된 폴더로 진입
+터미널에서 cd /goinfre/intra id/vm 이름으로 가상머신이 설치된 폴더로 진입
 shasum vm이름.vdi 를 실행
 잠시 기다리면 가상머신의 서명이 나옴
 여러차례 실행하면서 바뀌는지 확인
 shasum vm이름.vdi > signature.txt 에 넣고 git push 해서 평가준비
 
-
-
 https://velog.io/@joonpark/Born2BeRoot
-
 https://velog.io/@kyj93790/Born2beroot-4.-Vim-%EC%84%A4%EC%B9%98-%EB%B0%8F-SSH-%EC%84%A4%EC%A0%95
-
 https://velog.io/@tmdgks2222/42seoul-born2beroot-cron-monitoring.sh
-
 https://velog.io/@kyj93790/Born2beroot-6.-%EC%8B%9C%EC%8A%A4%ED%85%9C-%EC%A0%95%EB%B3%B4-%EC%B6%9C%EB%A0%A5-cron-tab
-
 https://velog.io/@garoulata/Born2beRoot
