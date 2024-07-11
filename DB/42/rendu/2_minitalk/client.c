149406648a9bfd0107cd0b01fa3d58c77f6c6286
메를로 <keunykim@c2r2s3.42gyeongsan.kr>
2024-07-11 23:56:26 +0900

    Thu_Jul_11_11:56:26

--- DB/42/rendu/2_minitalk/client.c
+++ DB/42/rendu/2_minitalk/client.c
@@ -8,10 +8,12 @@ static void	handler(int sig)
 		ft_printf("%d", sig / 11);
 }
 
-static void	send_byte(int pid, unsigned char byte)
+static void	send_byte(int pid, unsigned char byte, int slow)
 {
 	int	i;
 
+	if (!slow)
+		usleep(slow);
 	ft_printf("%d\t%c\t%d\t", byte, byte, ++count);
 	i = 0;
 	while (i < 8)
@@ -33,15 +35,12 @@ int	main(int argc, char *argv[])
 	ft_printf("실행 중인 프로그램의( %s ) 프로세스 아이디( %d )\n", argv[0], getpid());
 	pid = ft_atoi(argv[1]);
 	msg = argv[2];
-
 	signal(SIGUSR1, handler);
 	signal(SIGUSR2, handler);
-
 	while (*msg != '\0')
-		send_byte(pid, *(msg++));
-	send_byte(pid, '\0');
-
-	ft_printf("서버의 처리를 기다리는 중...\n");
+		send_byte(pid, *(msg++), 0);
+	send_byte(pid, '\0', 0);
+	ft_printf("기다리는 중...\n");
 	sleep(1);
 	return (0);
 }
