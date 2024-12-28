3d40e17c1d994a454c240557480e2e90ac9e7ca0
KKY <kky@n100>
2024-12-28 22:57:26 +0900

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #터미널 입력:  ./update.sh
 
+
 cp -r ~/dev/DB/프로젝트/게임/자바스크립트/빼빼로줍기/ .;
+
 git add .;
 git commit -m 'update';
 git push;
+
 clear;
 echo "확인(컨트롤 클릭): https://github.com/merleau42/public";
