39330be0a2b8e8a7b908e2322101adc4dbf76054
KKY <kky@n100>
2025-01-17 14:00:04 +0900

    devdevdev

--- DB/알고리즘/js/algo.js
+++ DB/알고리즘/js/algo.js
@@ -1,5 +1,5 @@
 //! 네임 스페이스 제거
-const { sqrt, ceil, floor, abs, sign, max, min } = Math;
+const { sqrt, ceil, floor, abs, sign, max, min, random } = Math;
 const { clear, log } = console;
 
 //!	입력값의 테스트 케이스를 불러와서 파싱
@@ -18,6 +18,7 @@ repl = (msg=log('REPL모드')) => require('repl').start();
 
 //!	배열 함수 확장, 문자열에 적용
 range = (a, l=0, d=1) => [...Array(abs(l - a)/d)].map((_,i)=>l ? a + d * i * sign(l - a) : d * i * sign(a));
+randz = (min, max, arr=0) => !arr ? floor(random() * (max - min + 1)) + min : range(arr).map(x => randz(min, max));
 arr_str = ['join', 'toReversed'];
 itertools = [
 	rank = function () {return this.map((x,i)=>[x*1,i]).toSorted((a,b)=>a[0]-b[0]).map((x,i)=>[x[0],i]).toSorted((a,b)=>a[0]-b[0]).map(x=>x[0])},
@@ -43,3 +44,5 @@ isPrime = (N)=> N>1 && N==2 || !range(2, ceil(sqrt(N)) + 1 ).some(i => N % i == 
 //! 메인
 seq = range(33, 40).prep();
 seq.sheet.show()
+
+randz(3,5,10).print();
