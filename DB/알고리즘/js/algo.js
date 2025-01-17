5019fff53d7e3664f53fa080e539d859efbab440
KKY <kky@n100>
2025-01-17 13:24:30 +0900

    D+R

--- DB/알고리즘/js/algo.js
+++ DB/알고리즘/js/algo.js
@@ -10,13 +10,14 @@ input = require("fs").readFileSync("./dev/stdin").toString()
 // if (input.pop() == "") input.pop(); // 마지막 줄 제거(EOF 등)
 
 //!	기본 함수, 디버깅 도구
-Object.prototype.print = function (s) { log(s==undefined ? this.valueOf() : this.join(s)); return this };
 Object.prototype.Each = function (f) { this.forEach(f); return this };
+Object.prototype.print = function (s) { log(s==undefined ? this.valueOf() : this.join(s)); return this };
+Object.prototype.show = function (s) { log(Object.entries()) };
 stamp = (()=>{ let count = 0; return (msg='STAMP',tabs=0)=>log("\t".repeat(tabs) + msg + ':', ++count) })();
-table = (...info) => log( info.map(x=>x.join('\t')).join('\n') );
-repl = () => require('repl').start();
+obj_to_table = function (obs) { log( info.map(x=>x.join('\t')).join('\n') ) };
+repl = (msg=log('REPL모드')) => require('repl').start();
 
-//!	배열, 문자열 함수
+//!	배열 함수 확장, 문자열에 적용
 range = (a, l=0, d=1) => [...Array(abs(l - a)/d)].map((_,i)=>l ? a + d * i * sign(l - a) : d * i * sign(a));
 arr_str = ['join', 'toReversed'];
 itertools = [
@@ -29,10 +30,12 @@ itertools = [
 .concat(arr_str).Each(f => String.prototype[f] = function(...args) { return [...this][f](...args) } );
 
 //! 수열, 누적합, 구간합
-Array.prototype.pre = [];
+Array.prototype.sheet = {};
+Array.prototype.prep = function (arr=this) { this.serial(); this.psum(); }
+
 seqtools = [
-	serial = function () { return this.pre.index = this.map((_,x) => i)},
-	psum = function (arr=this) { return arr.pre.psum = arr.reduce((s,c,i) => [ ...s, c + (s[i-1]||0) ], []) },
+	serial = function () { return this.sheet.index = this.map((_,i) => i)},
+	psum = function (arr=this) { return arr.sheet.psum = arr.reduce((s,c,i) => [ ...s, c + (s[i-1]||0) ], []) },
 ].map(f => f.name).Each(f => Array.prototype[f] = globalThis[f]);
 
 //! 정수론
@@ -40,3 +43,6 @@ isPrime = (N)=> N>1 && N==2 || !range(2, ceil(sqrt(N)) + 1 ).some(i => N % i == 
 
 //! 메인
 seq = range(33, 40);
+seq.prep();
+log(seq.sheet)
+repl();
