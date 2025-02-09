836507a399229a11448e81b03e50cfc58e94e03c
KKY <kky@n100>
2025-02-10 01:17:05 +0900

    devdevdev

--- DB/알고리즘/js/algo.js
+++ DB/알고리즘/js/algo.js
@@ -80,4 +80,4 @@ Array.prototype.draw = function(f) { return this.map((row,i) => row.map((col,j) 
 
 
 //! 메인
-N = input(); log(+(!(year % 400) || !!(year % 100) && !(year % 4)));
+N = +input(); range(N+1).sum().log()
