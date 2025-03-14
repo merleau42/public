6a83719fff973f06f2975632809fe7283752f562
KKY <kky@n100>
2025-03-14 18:50:36 +0900

    devdevdev

--- DB/알고리즘/js/작성중.js
+++ DB/알고리즘/js/작성중.js
@@ -1,11 +1,11 @@
 백트래킹 = (state, choices, args, 해집합=[]) => {
-	if ('순서쌍이_해를_만족함 || 온전한_순서쌍을_이룸') {
+	if (completed = '순서쌍이_해를_만족함 || 온전한_순서쌍을_이룸') {
 		해집합.push([...state]);
 		return;
 	}
 	
     for (choice of choices) {
-		if ('곧바로_부정되지는_않는_미래인가?') {
+		if (unrejectd = '곧바로_부정되지는_않는_미래인가?') {
 			state.push(choice);
 			백트래킹(state, choices, args, 해집합);
 			state.pop();
