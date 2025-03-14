b82a432eea9bfcb31f91c2caada44f97f7ca876f
KKY <kky@n100>
2025-03-14 18:28:34 +0900

    devdevdev

--- DB/알고리즘/js/작성중.js
+++ DB/알고리즘/js/작성중.js
@@ -1,18 +1,18 @@
 백트래킹 = (state, choices, args, 해집합=[]) => {
 	if ('순서쌍이_해를_만족함 || 온전한_순서쌍을_이룸') {
-		console.log(state);
-		해집합.push(state);
+		해집합.push([...state]);
 		return;
 	}
 	
     for (choice of choices) {
-		state.push(choice);
-		if ('조기탈락을_면함')
+		if ('바로_부정되지_않은_미래인가?') {
+			state.push(choice);
 			백트래킹(state, choices, args, 해집합);
-		state.pop();
+			state.pop();
+		}
 	}
 
 	return 해집합;
 }
 
-백트래킹([], choices, args)
+백트래킹([], choices, args);
