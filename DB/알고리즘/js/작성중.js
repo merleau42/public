fec1ecb09dd0ff58169e787e939b8af378cc7190
KKY <kky@n100>
2025-03-14 18:52:18 +0900

    devdevdev

--- DB/알고리즘/js/작성중.js
+++ DB/알고리즘/js/작성중.js
@@ -1,4 +1,4 @@
-백트래킹 = (state, choices, args, 해집합=[]) => {
+백트래킹 = (state, choices, args, completed, unrejectd, 해집합=[]) => {
 	if (completed = '순서쌍이_해를_만족함 || 온전한_순서쌍을_이룸') {
 		해집합.push([...state]);
 		return;
@@ -15,4 +15,4 @@
 	return 해집합;
 }
 
-백트래킹([], choices, args);
+백트래킹([], choices, args, completed, unrejectd);
