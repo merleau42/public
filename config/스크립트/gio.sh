c4f6174e5d545396089268adab8ef5dc3730ae4f
co.suffer.struggle <co.suffer.struggle@gmail.com>
2025-07-08 09:00:26 +0000

    Auto push

--- config/스크립트/gio.sh
+++ config/스크립트/gio.sh
@@ -1,5 +1,5 @@
 clear;
-echo '▲github.io 실행 '"$(TZ='Asia/Seoul' date '+%Y-%m-%d %H:%M:%S')" >> ~/dev/config/스크립트/.log
+echo '▲github.io 실행 '"$(date -u -d '+9 hours' '+%Y-%m-%d %H:%M:%S')" >> ~/dev/config/스크립트/.log
 
 ## 서브 저장소에 업로드할 내용을, 커밋 역할 폴더에 복사하기
 # 게임
