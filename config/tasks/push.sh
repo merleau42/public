07a49d2d01b2d213e0fe855918be5cf2dd104ae5
co.suffer.struggle <co.suffer.struggle@gmail.com>
2025-07-08 06:27:02 +0000

    Auto push

--- config/tasks/push.sh
+++ config/tasks/push.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-cd ~/dev || exit 1
-git add .
-git commit -m "▲DEV"
-git push
-
-# 종료를 유도하는 명령
-echo "작업 완료. 터미널은 자동으로 닫힙니다"
-sleep 1
-exit
+clear;
+echo '▲DEV 실행';
+cd ~/dev;
+git add .;
+git commit -m 'Auto push';
+git push; exit
