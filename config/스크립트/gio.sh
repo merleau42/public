7a02a194124b9e49cd4cdcdc68369c6c6403f144
co.suffer.struggle <co.suffer.struggle@gmail.com>
2025-07-23 07:45:56 +0000

    ▲GIO_2025-07-23_16:45:55

--- config/스크립트/gio.sh
+++ config/스크립트/gio.sh
@@ -9,8 +9,8 @@ GAME=~/dev/프로젝트/게임;
 
 GIO=~/dev/subtree/github.io;
 
-## 서브 저장소에 업로드할 내용을, 커밋 역할 폴더에 복사하기
-## index.html에 관련 항목을 반영할 것 (subtree/github.io/index.html)
+## [ github.io ]에 업로드할 내용을, 서브 폴더에 복사하기
+## 관련 항목을 index.html에 반영할 것 (subtree/github.io/index.html)
 # 게임
 	LIAR_GAME="$GAME/바닐라JS/라이어게임";
 		cp -r "$LIAR_GAME/단어수집기"		"$GIO/게임/라이어게임";
@@ -33,12 +33,13 @@ GIO=~/dev/subtree/github.io;
 		cp -r "$WEB/릴레이/연속변환기/"		  "$GIO/프론트엔드";
 		cp -r "$WEB/계산기/"				 "$GIO/프론트엔드";
 
-## 로컬 저장소의 전체 파일을 메인 저장소에 먼저 add/commit/push 시킴.
-## 일부 디렉토리만 add/commit 해도 되지만, 하나하나 지정하기 번거로움.
+## 로컬 디렉토리의 전부를, 메인 저장소에 먼저 add/commit/push 시킴.
+## 로컬 디렉토리의 일부만 add/commit 해도 되지만, 하나하나 지정하기 번거로움.
 clear; cd ~/dev; git add .; git commit -m "▲GIO_$time_kst"; git push;
 
-## 로컬 저장소의 일부 디렉토리만 서브 저장소에 push 함.
+## 서브 폴더를 [ github.io ]에 push 함.
+## 로컬 파일의 삭제 여부가 저장소에는 반영되지 않는 경우 --force 플래그를 사용.
 REV=$(git subtree split --prefix=subtree/github.io)
-git push https://github.com/merleau42/public $REV:main
+git push https://github.com/merleau42/public $REV:main --force
 
 exit 0;
