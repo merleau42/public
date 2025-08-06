67de5c189151e95b42078db640dd426e9ef18f87
co.suffer.struggle <co.suffer.struggle@gmail.com>
2025-08-06 03:04:23 +0000

    ▲PUSH 2025-08-06 12:04:21

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -25,21 +25,21 @@
     ];
 
     # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
+    # previews = {
+    #   enable = true;
+    #   previews = {
+    #     web = {
+    #       # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+    #       # and show it in IDX's web preview panel
+    #       command = ["npm" "run" "dev"];
+    #       manager = "web";
+    #       env = {
+    #         # Environment variables to set for your server
+    #         PORT = "$PORT";
+    #       };
+    #     };
+    #   };
+    # };
 
     # Workspace lifecycle hooks
     workspace = {
