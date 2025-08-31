03584b303f7e5fc66c198dfba44e08970860ebc5
co.suffer.struggle <co.suffer.struggle@gmail.com>
2025-08-31 12:23:25 +0000

    ▲PUSH 2025-08-31 21:23:23

--- config/스크립트/pull.sh
+++ config/스크립트/pull.sh
@@ -3,7 +3,7 @@
 clear;
 
 time_kst=$(date -u -d '+9 hours' '+%Y-%m-%d %H:%M:%S');
-echo "[실행] ▼PULL\t$time_kst" >> ~/dev/config/스크립트/.log;
+echo "[실행] ▼PULL\t$time_kst;" # pull은 로그 남기지 않기;
 cd ~/dev;
 git pull;
 exit 0;
